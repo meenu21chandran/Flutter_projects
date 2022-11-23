f9073b6844a488b7cb4cf1a2acc50dd8152f9776
meenu <meenu2001chandran@gmail.com>
2022-11-23 11:44:42 +0530

    commit

---
+++ lib/progress indicator/progress indicator.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: progressindicator (),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
+class progressindicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Loader(),
+    );
+  }
+}
+
+class Loader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vaccination Page'),
+        backgroundColor: Color(0xFF4CAF50),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              backgroundColor: Colors.redAccent,
+              valueColor: AlwaysStoppedAnimation(Colors.green),
+              strokeWidth: 10,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            LinearProgressIndicator(
+              backgroundColor: Colors.redAccent,
+              valueColor: AlwaysStoppedAnimation(Colors.green),
+              minHeight: 20,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
